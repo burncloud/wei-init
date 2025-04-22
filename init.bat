a4155128d62f3610d4d9ea235168782da44c5762
Wei <huangwei@burncloud.com>
2025-04-22 13:34:10 +0800

    修改：适配windows

--- init.bat
+++ init.bat
@@ -1,5 +1,31 @@
 @echo off
 
+git clone git@github.com:burncloud/wei.git
+git clone git@github.com:burncloud/wei-build.git
+git clone git@github.com:burncloud/wei-windows.git
+git clone git@github.com:burncloud/wei-run.git
+git clone git@github.com:burncloud/wei-server.git
+git clone git@github.com:burncloud/wei-release.git
+git clone git@github.com:burncloud/wei-updater.git
+git clone git@github.com:burncloud/wei-daemon.git
+git clone git@github.com:burncloud/wei-download.git
+git clone git@github.com:burncloud/wei-hardware.git
+git clone git@github.com:burncloud/wei-forward.git
+git clone git@github.com:burncloud/wei-task.git
+git clone git@github.com:burncloud/wei-docker.git
+git clone git@github.com:burncloud/wei-docker-linux.git
+git clone git@github.com:burncloud/wei-scheduler.git
+git clone git@github.com:burncloud/wei-dfdaemon.git
+git clone git@github.com:burncloud/wei-coin.git
+git clone git@github.com:burncloud/wei-shell.git
+git clone git@github.com:burncloud/wei-network.git
+git clone git@github.com:burncloud/wei-api.git
+git clone git@github.com:burncloud/wei-file.git
+git clone git@github.com:burncloud/wei-disk.git
+git clone git@github.com:burncloud/wei-docker-install.git
+
+
+
 :: Check for admin privileges
 net session >nul 2>&1
 if %errorlevel% neq 0 (
@@ -54,17 +80,17 @@ if %errorlevel% neq 0 (
 where openssl >nul 2>nul
 if %errorlevel% neq 0 (
     echo Installing OpenSSL...
-    choco install openssl -y
+    choco install openssl.light -y
+    set "OPENSSL_DIR=C:\Program Files\OpenSSL-Win64"
+    set "OPENSSL_LIB_DIR=C:\Program Files\OpenSSL-Win64\lib"
+    set "OPENSSL_INCLUDE_DIR=C:\Program Files\OpenSSL-Win64\include"
     set "PATH=%PATH%;C:\Program Files\OpenSSL-Win64\bin"
-    setx PATH "%PATH%"
+    setx OPENSSL_DIR "%OPENSSL_DIR%" /M
+    setx OPENSSL_LIB_DIR "%OPENSSL_LIB_DIR%" /M
+    setx OPENSSL_INCLUDE_DIR "%OPENSSL_INCLUDE_DIR%" /M
+    setx PATH "%PATH%" /M
 )
 
 echo Verifying Perl and OpenSSL installation...
 perl --version
 openssl version
-
-git clone git@github.com:burncloud/wei.git
-git clone git@github.com:burncloud/wei-build.git
-git clone git@github.com:burncloud/wei-windows.git
-git clone git@github.com:burncloud/wei-run.git
-git clone git@github.com:burncloud/wei-server.git
